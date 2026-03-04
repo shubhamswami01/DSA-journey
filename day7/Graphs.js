9783b2bd62486dd4ac367c2dfa98d932c7341eb2
Shubham Swami <159449971+shubhamswami01@users.noreply.github.com>
2026-03-04 20:51:03 +0530

    Create day7/Graphs.js

---
+++ day7/Graphs.js
@@ -0,0 +1,32 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1); // undirected graph
+  }
+
+  show() {
+    console.log(this.adjacencyList);
+  }
+}
+
+// Testing
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+
+graph.show();
